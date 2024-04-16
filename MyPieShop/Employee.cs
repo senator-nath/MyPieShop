89bfe20a9eba00e9ac7e68cf1a74659a67d090c7
senator-nath <nathanielosana18@gmail.com>
2024-04-16 09:25:51 +0100

    Add project files.

---
+++ MyPieShop/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPieShop
+{
+    internal class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numbersOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+
+        public DateTime birthday;
+
+        const int minimalHourWorked = 1;
+
+        public void performWork()
+        {
+            performWork(minimalHourWorked);
+        }
+        public void performWork(int numberOfHours)
+        {
+            numbersOfHoursWorked += numberOfHours;
+            Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHours} hour(s)!");
+        }
+        public double recieveWage(bool restHours = true)
+        {
+            wage = numbersOfHoursWorked * hourlyRate;
+            Console.WriteLine($"{firstName} {lastName} has received a wage of {wage} for{numbersOfHoursWorked} hour(s) of work");
+            if (restHours)
+
+                numbersOfHoursWorked = 0;
+            return wage;
+
+        }
+        public void displayEmployeeDetails()
+        {
+            Console.WriteLine($"\nfirst name:\t{firstName} \nlast name: \t{lastName} \nemail: \t{email} \nbirthday:\t{birthday.ToShortDateString()}")};
+    }
+
+
+}
---
+++ MyPieShop/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MyPieShop;
+
+//int amount = 1248;
+//int month = 12;
+//int bonus = 1000;
+
+//int yearlyWage = Utilities.calculateYearlyWage(amount, month, bonus);
+//Console.WriteLine($"yraely wage is : {yearlyWage}");
+
+Utilities.usingEscapeCharacters();
+
+
+
+
+
+
+
+
+
+
+
+//do
+//{
+//    Console.WriteLine("choose the action you want to do");
+//    Console.WriteLine("enter 1 to add employee");
+//    Console.WriteLine("enter 2 to update employee");
+//    Console.WriteLine("enter 3 to delete employee");
+//    Console.WriteLine("enter 99 to exit application");
+//    string selectAction = Console.ReadLine();
+
+//    switch (selectAction)
+//    {
+//        case "1":
+//            Console.WriteLine("adding employee"); break;
+//        case "2":
+//            Console.WriteLine("updating employee"); break;
+//        case "3":
+//            Console.WriteLine("deleting employee"); break;
+//        default:
+//            Console.WriteLine("invalid input"); break;
+//    }
+//} while (selectAction != "99");
+
+//Console.WriteLine("exiting application");
+
